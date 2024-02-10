439338532d5ebabbef1722857744415d2d935168
iam-vl <gztrk@engineer.com>
2024-02-10 12:52:52 +0400

    C007 Add chi router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,20 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+
+	// "github.com/go-chi/chi/v5"
 	"github.com/iam-vl/go-web/pkg/config"
 	"github.com/iam-vl/go-web/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// Multiplexer
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/iam-vl/go-web
 
 go 1.21.4
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.11
+)
